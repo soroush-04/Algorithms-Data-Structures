c20e1d1a61fd0df2d4e86ca9a138c8868b0b3257
Soroush Kami <soroushkami10@gmail.com>
2024-09-20 18:54:45 -0400

    maximum BinaryGap - brute force

--- Main.py
+++ Main.py
@@ -1,18 +1,49 @@
-"""Let’s print a triangle made of asterisks (‘*’) separated by spaces and consisting
-of n rows again, but this time upside down, and make it symmetrical. Consecutive rows should
-contain 2n − 1, 2n − 3, . . . , 3, 1 asterisks and should be indented by 0, 2, 4, . . . , 2(n − 1)
-spaces. For example, for n = 4 the triangle should appear as follows:
-* * * * * * *
-  * * * * *
-    * * *
-      * 
+"""
+A binary gap within a positive integer N is any maximal sequence of consecutive zeros 
+that is surrounded by ones at both ends in the binary representation of N.
+
+For example, number 9 has binary representation 1001 and contains a binary gap of length 2. 
+The number 529 has binary representation 1000010001 and contains two binary gaps: one of length 4 and one of length 3. 
+The number 20 has binary representation 10100 and contains one binary gap of length 1. 
+The number 15 has binary representation 1111 and has no binary gaps. The number 32 has binary representation 100000 and has no binary gaps.
+
+Write a function:
+
+def solution(N)
+
+that, given a positive integer N, returns the length of its longest binary gap. 
+The function should return 0 if N doesn't contain a binary gap.
+
+For example, given N = 1041 the function should return 5, because N has binary representation 10000010001 
+and so its longest binary gap is of length 5.
+Given N = 32 the function should return 0, because N has binary representation '100000' and thus no binary gaps.
+
+Write an efficient algorithm for the following assumptions:
+
+N is an integer within the range [1..2,147,483,647].
 """
 
 
-def triangle(n):
-    for i in range(n, 0, -1):
-        print(" " * (2 * (n - i)), end="")
-        print("* " * (2 * i - 1))
+def solution(n: int):
+    rev_solution = []
+    max_gap = 0
+    temp_gap = 0
+
+    while n > 0:
+        rev_solution.append(n % 2)
+        n //= 2
+
+    rev_solution.reverse()
+
+    for i in rev_solution:
+        if i == 0:
+            temp_gap += 1
+
+        elif i == 1:
+            max_gap = max(temp_gap, max_gap)
+            temp_gap = 0
+
+    return print([rev_solution, max_gap])
 
 
-triangle(4)
+solution(int(input("enter number: ")))
