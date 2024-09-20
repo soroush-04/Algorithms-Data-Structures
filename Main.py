9d0723f35ac5e49267216c5db9f731c3a033d5dc
Soroush Kami <soroushkami10@gmail.com>
2024-09-20 16:56:10 -0400

    triangle of asterisks - brute force

--- Main.py
+++ Main.py
@@ -1,1 +1,17 @@
+"""
+Let’s print a triangle made of asterisks (‘*’) separated by spaces. The triangle
+should consist of n rows, where n is a given positive integer, and consecutive rows should
+contain 1, 2, . . . , n asterisks. For example, for n = 4 the triangle should appear as follows:
+"""
 
+
+def triangle(n: int):
+    for i in range(1, n + 1):
+        for j in range(i):
+            print("*", end="")
+        print()
+
+    return None
+
+
+triangle(2)
