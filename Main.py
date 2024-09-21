1c7dcb97fbe6cd451159c1f82ce2bd24e2a84de6
Soroush Kami <soroushkami10@gmail.com>
2024-09-20 22:47:35 -0400

    Tape equilibrium - brute force

--- Main.py
+++ Main.py
@@ -1,35 +1,57 @@
-"""An array A consisting of N different integers is given. The array contains integers in the range [1..(N + 1)], which means that exactly one element is missing.
+"""A non-empty array A consisting of N integers is given. Array A represents numbers on a tape.
 
-Your goal is to find that missing element.
+Any integer P, such that 0 < P < N, splits this tape into two non-empty parts: A[0], A[1], ..., A[P − 1] and A[P], A[P + 1], ..., A[N − 1].
 
+The difference between the two parts is the value of: |(A[0] + A[1] + ... + A[P − 1]) − (A[P] + A[P + 1] + ... + A[N − 1])|
+
+In other words, it is the absolute difference between the sum of the first part and the sum of the second part.
+
+For example, consider array A such that:
+
+  A[0] = 3
+  A[1] = 1
+  A[2] = 2
+  A[3] = 4
+  A[4] = 3
+We can split this tape in four places:
+
+P = 1, difference = |3 − 10| = 7
+P = 2, difference = |4 − 9| = 5
+P = 3, difference = |6 − 7| = 1
+P = 4, difference = |10 − 3| = 7
 Write a function:
 
 def solution(A)
 
-that, given an array A, returns the value of the missing element.
+that, given a non-empty array A of N integers, returns the minimal difference that can be achieved.
 
-For example, given array A such that:
+For example, given:
 
-  A[0] = 2
-  A[1] = 3
-  A[2] = 1
-  A[3] = 5
-the function should return 4, as it is the missing element.
+  A[0] = 3
+  A[1] = 1
+  A[2] = 2
+  A[3] = 4
+  A[4] = 3
+the function should return 1, as explained above.
+
 Write an efficient algorithm for the following assumptions:
 
-N is an integer within the range [0..100,000];
-the elements of A are all distinct;
-each element of array A is an integer within the range [1..(N + 1)].
-"""
+N is an integer within the range [2..100,000];
+each element of array A is an integer within the range [−1,000..1,000]."""
 
 from typing import List
+import math
 
 
-def solution(A: List[int]) -> int:
-    full_A = range(1, len(A) + 2)
+def solution(A: List[int]):
+    all_min = sum(A)
 
-    return sum(full_A) - sum(A)
+    for i in range(len(A) - 1):
+        temp_min = abs(sum(A[: i + 1]) - sum(A[i + 1 :]))
+        all_min = min(all_min, temp_min)
+
+    return all_min
 
 
-array = [1, 2, 3, 6, 8, 7, 5]
+array = [3, 1, 2, 4, 3]
 print(solution(array))
