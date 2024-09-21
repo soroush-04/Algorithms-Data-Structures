9c8833bf0d27bd580382de22e404bcf2ec7c502c
Soroush Kami <soroushkami10@gmail.com>
2024-09-20 20:23:03 -0400

    Cyclic rotation - optimized

--- Main.py
+++ Main.py
@@ -1,49 +1,54 @@
 """
-A binary gap within a positive integer N is any maximal sequence of consecutive zeros 
-that is surrounded by ones at both ends in the binary representation of N.
+    An array A consisting of N integers is given. Rotation of the array means that each element is shifted right by one index, and the last element of the array is moved to the first place. For example, the rotation of array A = [3, 8, 9, 7, 6] is [6, 3, 8, 9, 7] (elements are shifted right by one index and 6 is moved to the first place).
 
-For example, number 9 has binary representation 1001 and contains a binary gap of length 2. 
-The number 529 has binary representation 1000010001 and contains two binary gaps: one of length 4 and one of length 3. 
-The number 20 has binary representation 10100 and contains one binary gap of length 1. 
-The number 15 has binary representation 1111 and has no binary gaps. The number 32 has binary representation 100000 and has no binary gaps.
+The goal is to rotate array A K times; that is, each element of A will be shifted to the right K times.
 
 Write a function:
 
-def solution(N)
+def solution(A, K)
 
-that, given a positive integer N, returns the length of its longest binary gap. 
-The function should return 0 if N doesn't contain a binary gap.
+that, given an array A consisting of N integers and an integer K, returns the array A rotated K times.
 
-For example, given N = 1041 the function should return 5, because N has binary representation 10000010001 
-and so its longest binary gap is of length 5.
-Given N = 32 the function should return 0, because N has binary representation '100000' and thus no binary gaps.
+For example, given
 
-Write an efficient algorithm for the following assumptions:
+    A = [3, 8, 9, 7, 6]
+    K = 3
+the function should return [9, 7, 6, 3, 8]. Three rotations were made:
 
-N is an integer within the range [1..2,147,483,647].
+    [3, 8, 9, 7, 6] -> [6, 3, 8, 9, 7]
+    [6, 3, 8, 9, 7] -> [7, 6, 3, 8, 9]
+    [7, 6, 3, 8, 9] -> [9, 7, 6, 3, 8]
+For another example, given
+
+    A = [0, 0, 0]
+    K = 1
+the function should return [0, 0, 0]
+
+Given
+
+    A = [1, 2, 3, 4]
+    K = 4
+the function should return [1, 2, 3, 4]
+
+Assume that:
+
+N and K are integers within the range [0..100];
+each element of array A is an integer within the range [−1,000..1,000].
+In your solution, focus on correctness. The performance of your solution will not be the focus of the assessment.
 """
 
-
-def solution(n: int):
-    rev_solution = []
-    max_gap = 0
-    temp_gap = 0
-
-    while n > 0:
-        rev_solution.append(n % 2)
-        n //= 2
-
-    rev_solution.reverse()
-
-    for i in rev_solution:
-        if i == 0:
-            temp_gap += 1
-
-        elif i == 1:
-            max_gap = max(temp_gap, max_gap)
-            temp_gap = 0
-
-    return print([rev_solution, max_gap])
+from typing import List
 
 
-solution(int(input("enter number: ")))
+def solution(A: List[int], K: int):
+    if A == [] or K == 0:
+        return A
+
+    # optimized rotation
+    K %= len(A)
+
+    return A[-K:] + A[:-K]
+
+
+array = [1, 2, 3, 4, 5, 6]
+print(solution(array, 2))
