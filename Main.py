60b0698428797e75ed08076349c08988442641f1
Soroush Kami <soroushkami10@gmail.com>
2024-09-21 13:14:03 -0400

    Missing integer N logN

--- Main.py
+++ Main.py
@@ -1,79 +1,33 @@
-"""You are given N counters, initially set to 0, and you have two possible operations on them:
+"""Write a function:
 
-increase(X) − counter X is increased by 1,
-max counter − all counters are set to the maximum value of any counter.
-A non-empty array A of M integers is given. This array represents consecutive operations:
+def solution(A)
 
-if A[K] = X, such that 1 ≤ X ≤ N, then operation K is increase(X),
-if A[K] = N + 1 then operation K is max counter.
-For example, given integer N = 5 and array A such that:
+that, given an array A of N integers, returns the smallest positive integer (greater than 0) that does not occur in A.
 
-    A[0] = 3
-    A[1] = 4
-    A[2] = 4
-    A[3] = 6
-    A[4] = 1
-    A[5] = 4
-    A[6] = 4
-the values of the counters after each consecutive operation will be:
+For example, given A = [1, 3, 6, 4, 1, 2], the function should return 5.
 
-    (0, 0, 1, 0, 0)
-    (0, 0, 1, 1, 0)
-    (0, 0, 1, 2, 0)
-    (2, 2, 2, 2, 2)
-    (3, 2, 2, 2, 2)
-    (3, 2, 2, 3, 2)
-    (3, 2, 2, 4, 2)
-The goal is to calculate the value of every counter after all operations.
+Given A = [1, 2, 3], the function should return 4.
 
-Write a function:
-
-def solution(N, A)
-
-that, given an integer N and a non-empty array A consisting of M integers, returns a sequence of integers representing the values of the counters.
-
-Result array should be returned as an array of integers.
-
-For example, given:
-
-    A[0] = 3
-    A[1] = 4
-    A[2] = 4
-    A[3] = 6
-    A[4] = 1
-    A[5] = 4
-    A[6] = 4
-the function should return [3, 2, 2, 4, 2], as explained above.
+Given A = [−1, −3], the function should return 1.
 
 Write an efficient algorithm for the following assumptions:
 
-N and M are integers within the range [1..100,000];
-each element of array A is an integer within the range [1..N + 1].
-"""
-
-from typing import List
+N is an integer within the range [1..100,000];
+each element of array A is an integer within the range [−1,000,000..1,000,000]."""
 
 
-def solution(N: int, A: List[int]):
-    counter = [0] * N
-    total_max = 0
-    temp_max = 0
+def solution(A):
+    temp = 1
 
-    for num in A:
-        if num <= N:
-            if counter[num - 1] < temp_max:
-                counter[num - 1] = temp_max
-            counter[num - 1] += 1
-            total_max = max(counter[num - 1], total_max)
-        else:
-            temp_max = total_max
+    if max(A) < 1:
+        return 1
 
-    for i in range(N):
-        if counter[i] < temp_max:
-            counter[i] = temp_max
-
-    return counter
+    for num in sorted(A):
+        if num == temp:
+            temp += 1
+        elif num > temp:
+            return temp
 
 
-array = [3, 4, 4, 6, 1, 4, 4]
-print(solution(5, array))
+array = [1, 5, 3, 2, 8, 7, 0]
+print(solution(array))
