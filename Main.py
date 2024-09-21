30a49e17b09a87c77d69216f6834c818f520a6ca
Soroush Kami <soroushkami10@gmail.com>
2024-09-21 13:58:43 -0400

    Mushroom picker optimized

--- Main.py
+++ Main.py
@@ -1,30 +1,58 @@
-"""Write a function:
+"""You are given a non-empty, zero-indexed array A of n (1 � n � 100 000) integers
+a0, a1, . . . , an−1 (0 � ai � 1 000). This array represents number of mushrooms growing on the
+consecutive spots along a road. You are also given integers k and m (0 � k, m < n).
+A mushroom picker is at spot number k on the road and should perform m moves. In
+one move she moves to an adjacent spot. She collects all the mushrooms growing on spots
+she visits. The goal is to calculate the maximum number of mushrooms that the mushroom
+picker can collect in m moves.
+For example, consider array A such that:
+2 3 7 5 1 3 9
+0 1 2 3 4 5 6
+The mushroom picker starts at spot k = 4 and should perform m = 6 moves. She might
+move to spots 3, 2, 3, 4, 5, 6 and thereby collect 1 + 5 + 7 + 3 + 9 = 25 mushrooms. This is the
+maximal number of mushrooms she can collect."""
 
-def solution(A)
-
-that, given an array A of N integers, returns the smallest positive integer (greater than 0) that does not occur in A.
-
-For example, given A = [1, 3, 6, 4, 1, 2], the function should return 5.
-
-Given A = [1, 2, 3], the function should return 4.
-
-Given A = [−1, −3], the function should return 1.
-
-Write an efficient algorithm for the following assumptions:
-
-N is an integer within the range [1..100,000];
-each element of array A is an integer within the range [−1,000,000..1,000,000]."""
-
-
-def solution(A):
-    seen = set(A)
-    temp = 1
-
-    while temp in seen:
-        temp += 1
-
-    return temp
+from typing import List
 
 
-array = [1, 5, 3, 2, 8, 7, 0]
-print(solution(array))
+def prefix_sums(A: List[int]) -> List[int]:
+    n = len(A)
+    P = [0] * n
+    P[0] = A[0]
+
+    for i in range(1, n):
+        P[i] = P[i - 1] + A[i]
+
+    return P
+
+
+def count_total(pref: List[int], left: int, right: int) -> int:
+    if left > right:
+        return 0
+    return pref[right] - (pref[left - 1] if left > 0 else 0)
+
+
+def mushrooms(A: List[int], k: int, m: int) -> int:
+    n = len(A)
+    result = 0
+    pref = prefix_sums(A)
+
+    # Picking mushrooms from the left
+    for p in range(min(m, k) + 1):
+        left_pos = k - p
+        right_pos = min(n - 1, max(k, k + m - 2 * p))
+        result = max(result, count_total(pref, left_pos, right_pos))
+
+    # Picking mushrooms from the right
+    for p in range(min(m + 1, n - k)):
+        right_pos = k + p
+        left_pos = max(0, min(k, k - (m - 2 * p)))
+        result = max(result, count_total(pref, left_pos, right_pos))
+
+    return result
+
+
+A = [2, 3, 7, 5, 1, 3, 9]
+k = 4
+m = 6
+print(mushrooms(A, k, m))
