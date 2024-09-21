0b154117ec64738f1bf3650e7b8d2bc60f2913aa
Soroush Kami <soroushkami10@gmail.com>
2024-09-20 21:45:36 -0400

    Frog jump - brute force

--- Main.py
+++ Main.py
@@ -1,49 +1,38 @@
-"""A non-empty array A consisting of N integers is given. The array contains an odd number of elements, and each element of the array can be paired with another element that has the same value, except for one element that is left unpaired.
+"""A small frog wants to get to the other side of the road. The frog is currently located at position X and wants to get to a position greater than or equal to Y. The small frog always jumps a fixed distance, D.
 
-For example, in array A such that:
+Count the minimal number of jumps that the small frog must perform to reach its target.
 
-  A[0] = 9  A[1] = 3  A[2] = 9
-  A[3] = 3  A[4] = 9  A[5] = 7
-  A[6] = 9
-the elements at indexes 0 and 2 have value 9,
-the elements at indexes 1 and 3 have value 3,
-the elements at indexes 4 and 6 have value 9,
-the element at index 5 has value 7 and is unpaired.
 Write a function:
 
-def solution(A)
+def solution(X, Y, D)
 
-that, given an array A consisting of N integers fulfilling the above conditions, returns the value of the unpaired element.
+that, given three integers X, Y and D, returns the minimal number of jumps from position X to a position equal to or greater than Y.
 
-For example, given array A such that:
+For example, given:
 
-  A[0] = 9  A[1] = 3  A[2] = 9
-  A[3] = 3  A[4] = 9  A[5] = 7
-  A[6] = 9
-the function should return 7, as explained in the example above.
+  X = 10
+  Y = 85
+  D = 30
+the function should return 3, because the frog will be positioned as follows:
 
+after the first jump, at position 10 + 30 = 40
+after the second jump, at position 10 + 30 + 30 = 70
+after the third jump, at position 10 + 30 + 30 + 30 = 100
 Write an efficient algorithm for the following assumptions:
 
-N is an odd integer within the range [1..1,000,000];
-each element of array A is an integer within the range [1..1,000,000,000];
-all but one of the values in A occur an even number of times.
+X, Y and D are integers within the range [1..1,000,000,000];
+X ≤ Y.
 """
 
-from typing import List
+
+def solution(X: int, Y: int, D: int):
+    jump = 0
+
+    while X < Y:
+        X += D
+        jump += 1
+
+    return jump
 
 
-def solution(A: List[int]):
-    unpaired = {}
-
-    for num in A:
-        if num in unpaired:
-            del unpaired[num]
-        else:
-            unpaired[num] = True
-
-    return next(iter(unpaired.keys()), None)
-
-
-array = [1, 2, 1, 2, 3, 3, 4, 5, 4]
-
-print(solution(array))
+print(solution(10, 85, 30))
