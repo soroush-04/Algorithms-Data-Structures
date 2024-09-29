62630120b9bb315701994d6963efb28ab978e63f
Soroush Kami <soroushkami10@gmail.com>
2024-09-29 14:46:02 -0400

    Count div

--- Main.py
+++ Main.py
@@ -1,56 +1,20 @@
-"""A non-empty array A consisting of N integers is given. The consecutive elements of array A represent consecutive cars on a road.
+"""Write a function:
 
-Array A contains only 0s and/or 1s:
+def solution(A, B, K)
 
-0 represents a car traveling east,
-1 represents a car traveling west.
-The goal is to count passing cars. We say that a pair of cars (P, Q), where 0 ≤ P < Q < N, is passing when P is traveling to the east and Q is traveling to the west.
+that, given three integers A, B and K, returns the number of integers within the range [A..B] that are divisible by K, i.e.:
 
-For example, consider array A such that:
+{ i : A ≤ i ≤ B, i mod K = 0 }
 
-  A[0] = 0
-  A[1] = 1
-  A[2] = 0
-  A[3] = 1
-  A[4] = 1
-We have five pairs of passing cars: (0, 1), (0, 3), (0, 4), (2, 3), (2, 4).
-
-Write a function:
-
-def solution(A)
-
-that, given a non-empty array A of N integers, returns the number of pairs of passing cars.
-
-The function should return −1 if the number of pairs of passing cars exceeds 1,000,000,000.
-
-For example, given:
-
-  A[0] = 0
-  A[1] = 1
-  A[2] = 0
-  A[3] = 1
-  A[4] = 1
-the function should return 5, as explained above.
+For example, for A = 6, B = 11 and K = 2, your function should return 3, because there are three numbers divisible by 2 within the range [6..11], namely 6, 8 and 10.
 
 Write an efficient algorithm for the following assumptions:
 
-N is an integer within the range [1..100,000];
-each element of array A is an integer that can have one of the following values: 0, 1.
-Copyright 2009–2024 by Codility Limited. All Rights Reserved. Unauthorized copying, publication or disclosure prohibited."""
+A and B are integers within the range [0..2,000,000,000];
+K is an integer within the range [1..2,000,000,000];
+A ≤ B.
+Copyright 2009–2024 by Codility Limited. All Rights Reserved. Unauthorized copying, publication or disclosure prohibited.
+"""
 
-from typing import List
-
-def solution(A: List[int]):
-    counter = 0
-    multiplier = 0
-    
-    for num in A:
-        if num == 0:
-            multiplier += 1
-        if num == 1:
-            counter = multiplier + counter
-    
-    if counter > 1_000_000_000: #1,000,000,000
-        return -1
-    else: 
-        return counter
+def solution(A, B, K):
+    return (B // K - (A-1) // K)
