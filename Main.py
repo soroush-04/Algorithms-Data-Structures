f3449385279a61678dc329e264a768ef198c7722
Soroush Kami <soroushkami10@gmail.com>
2024-09-30 00:34:40 -0400

    Fish stream

--- Main.py
+++ Main.py
@@ -1,41 +1,74 @@
-"""We draw N discs on a plane. The discs are numbered from 0 to N − 1. An array A of N non-negative integers, specifying the radiuses of the discs, is given. The J-th disc is drawn with its center at (J, 0) and radius A[J].
+"""You are given two non-empty arrays A and B consisting of N integers. Arrays A and B represent N voracious fish in a river, ordered downstream along the flow of the river.
 
-We say that the J-th disc and K-th disc intersect if J ≠ K and the J-th and K-th discs have at least one common point (assuming that the discs contain their borders).
+The fish are numbered from 0 to N − 1. If P and Q are two fish  and P < Q, then fish P is initially upstream of fish Q. Initially, each fish has a unique position.
 
-The figure below shows discs drawn for N = 6 and A as follows:
+Fish number P is represented by A[P] and B[P]. Array A contains the sizes of the fish. All its elements are unique. Array B contains the directions of the fish. It contains only 0s and/or 1s, where:
 
-  A[0] = 1
-  A[1] = 5
-  A[2] = 2
-  A[3] = 1
-  A[4] = 4
-  A[5] = 0
+0 represents a fish flowing upstream,
+1 represents a fish flowing downstream.
+If two fish move in opposite directions and there are no other (living) fish between them, they will eventually meet each other. Then only one fish can stay alive − the larger fish eats the smaller one. More precisely, we say that two fish P and Q meet each other when P < Q, B[P] = 1 and B[Q] = 0, and there are no living fish between them. After they meet:
 
+If A[P] > A[Q] then P eats Q, and P will still be flowing downstream,
+If A[Q] > A[P] then Q eats P, and Q will still be flowing upstream.
+We assume that all the fish are flowing at the same speed. That is, fish moving in the same direction never meet. The goal is to calculate the number of fish that will stay alive.
 
-There are eleven (unordered) pairs of discs that intersect, namely:
+For example, consider arrays A and B such that:
 
-discs 1 and 4 intersect, and both intersect with all the other discs;
-disc 2 also intersects with discs 0 and 3.
+  A[0] = 4    B[0] = 0
+  A[1] = 3    B[1] = 1
+  A[2] = 2    B[2] = 0
+  A[3] = 1    B[3] = 0
+  A[4] = 5    B[4] = 0
+Initially all the fish are alive and all except fish number 1 are moving upstream. Fish number 1 meets fish number 2 and eats it, then it meets fish number 3 and eats it too. Finally, it meets fish number 4 and is eaten by it. The remaining two fish, number 0 and 4, never meet and therefore stay alive.
+
 Write a function:
 
-def solution(A)
+def solution(A, B)
 
-that, given an array A describing N discs as explained above, returns the number of (unordered) pairs of intersecting discs. The function should return −1 if the number of intersecting pairs exceeds 10,000,000.
+that, given two non-empty arrays A and B consisting of N integers, returns the number of fish that will stay alive.
 
-Given array A shown above, the function should return 11, as explained above.
+For example, given the arrays shown above, the function should return 2, as explained above.
 
 Write an efficient algorithm for the following assumptions:
 
-N is an integer within the range [0..100,000];
-each element of array A is an integer within the range [0..2,147,483,647].
+N is an integer within the range [1..100,000];
+each element of array A is an integer within the range [0..1,000,000,000];
+each element of array B is an integer that can have one of the following values: 0, 1;
+the elements of A are all distinct.
 Copyright 2009–2024 by Codility Limited. All Rights Reserved. Unauthorized copying, publication or disclosure prohibited."""
 
-def solution(A):
-    counter = 0
+# from typing import List
+
+# def solution(A: List[int], B: List[int]):
+#     flow = B[1]
+#     stack = [A[0]]
     
+#     for i in range(1, len(A)):
+#         if B[i] != flow:
+#             if stack[-1] > A[i]:
+#                 pass
+#             else:
+#                 stack[-1] = A[i]
+#                 flow = B[i]
+#         else:
+#             stack.append(A[i])
+    
+#     return len(stack)
+            
+            
+from typing import List
+
+def solution(A: List[int], B: List[int]) -> int:
+    stack = []
+    survived = 0
+
     for i in range(len(A)):
-        for j in range(i+1, len(A)):
-            if i + A[i] >= j - A[j]:
-                counter += 1
+        if B[i] == 1:
+            stack.append(A[i])
+        else:
+            while stack and A[i] > stack[-1]:
+                stack.pop()
+            if not stack:  # If no downstream fish left to eat
+                survived += 1
                 
-    return counter
+    return len(stack) + survived
