68c45bf6adf37e47a694feda2680fe7b2f7e7048
Soroush Kami <soroushkami10@gmail.com>
2024-10-02 01:21:31 -0400

    Minimum Size Subarray Sum

--- Main.py
+++ Main.py
@@ -1,21 +1,50 @@
-"""Problem: Find the longest substring without repeating characters.
+"""Given an array of positive integers nums and a positive integer target, return the minimal length of a 
+subarray
+ whose sum is greater than or equal to target. If there is no such subarray, return 0 instead.
 
-"""
+ 
 
-from typing import List
+Example 1:
 
-def solution(S: List[str]):
-    subarray = set()
-    left = 0
-    overall_max = 0
-    
-    for right in range(len(S)):
-        while S[right] in subarray:
-            subarray.remove(S[left])
-            left += 1
-        subarray.add(S[right])
-        overall_max = max(overall_max, (right - left + 1))
-    
-    return overall_max
+Input: target = 7, nums = [2,3,1,2,4,3]
+Output: 2
+Explanation: The subarray [4,3] has the minimal length under the problem constraint.
+Example 2:
+
+Input: target = 4, nums = [1,4,4]
+Output: 1
+Example 3:
+
+Input: target = 11, nums = [1,1,1,1,1,1,1,1]
+Output: 0
+ 
+
+Constraints:
+
+1 <= target <= 109
+1 <= nums.length <= 105
+1 <= nums[i] <= 104
+ 
+
+Follow up: If you have figured out the O(n) solution, try coding another solution of which the time complexity is O(n log(n))."""
+
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+
+        if sum(nums) < target: 
+            return 0
             
+        min_length = float('inf')
+        total = 0
+        right = 0
+        left = 0
+        
+        for right in range(len(nums)):
+            total += nums[right]
+            while total >= target:
+                min_length = min(min_length, (right - left + 1))
+                total -= nums[left]
+                left += 1
             
+        
+        return min_length if min_length != float('inf') else 0
