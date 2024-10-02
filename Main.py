357b249e7512bee257ac25a3ebd84462b6f1d670
Soroush Kami <soroushkami10@gmail.com>
2024-10-02 16:13:36 -0400

    guess number

--- Main.py
+++ Main.py
@@ -1,42 +1,58 @@
-"""You are given an integer array nums consisting of n elements, and an integer k.
+"""We are playing the Guess Game. The game is as follows:
 
-Find a contiguous subarray whose length is equal to k that has the maximum average value and return this value. Any answer with a calculation error less than 10-5 will be accepted.
+I pick a number from 1 to n. You have to guess which number I picked.
+
+Every time you guess wrong, I will tell you whether the number I picked is higher or lower than your guess.
+
+You call a pre-defined API int guess(int num), which returns three possible results:
+
+-1: Your guess is higher than the number I picked (i.e. num > pick).
+1: Your guess is lower than the number I picked (i.e. num < pick).
+0: your guess is equal to the number I picked (i.e. num == pick).
+Return the number that I picked.
 
  
 
 Example 1:
 
-Input: nums = [1,12,-5,-6,50,3], k = 4
-Output: 12.75000
-Explanation: Maximum average is (12 - 5 - 6 + 50) / 4 = 51 / 4 = 12.75
+Input: n = 10, pick = 6
+Output: 6
 Example 2:
 
-Input: nums = [5], k = 1
-Output: 5.00000
+Input: n = 1, pick = 1
+Output: 1
+Example 3:
+
+Input: n = 2, pick = 1
+Output: 1
  
 
 Constraints:
 
-n == nums.length
-1 <= k <= n <= 105
--104 <= nums[i] <= 104"""
+1 <= n <= 231 - 1
+1 <= pick <= n"""
 
-from typing import List
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if num is higher than the picked number
+#          1 if num is lower than the picked number
+#          otherwise return 0
+# def guess(num: int) -> int:
 
 class Solution:
-    def findMaxAverage(self, nums: List[int], k: int) -> float:
-        sum = 0 
-        left = 0
-        max_sum = float('-inf')
+    def guessNumber(self, n: int) -> int:
+        left = 1
         
-        if len(nums) < k:
-            return 0
+        while left <= n:
+            my_guess = left + (n - left) // 2
+            check = guess(my_guess)
+            if check == 0:
+                return my_guess
+            elif check > 0:
+                left = my_guess + 1
+            else:
+                n = my_guess - 1
         
-        for right in range(len(nums)):
-            sum += nums[right]
-            if right - left + 1 == k:
-                max_sum = max(sum, max_sum)
-                sum -= nums[left]
-                left += 1
         
-        return max_sum / k
+                
+            
