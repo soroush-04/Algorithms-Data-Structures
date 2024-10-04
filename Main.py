e39aa62aa8ff06db032aff35a8f7d7fc15974900
Soroush Kami <soroushkami10@gmail.com>
2024-10-03 23:58:54 -0400

    Graphs practice 1

--- Main.py
+++ Main.py
@@ -1,43 +1,32 @@
-"""A peak element is an element that is strictly greater than its neighbors.
 
-Given a 0-indexed integer array nums, find a peak element, and return its index. If the array contains multiple peaks, return the index to any of the peaks.
-
-You may imagine that nums[-1] = nums[n] = -∞. In other words, an element is always considered to be strictly greater than a neighbor that is outside the array.
-
-You must write an algorithm that runs in O(log n) time.
-
- 
-
-Example 1:
-
-Input: nums = [1,2,3,1]
-Output: 2
-Explanation: 3 is a peak element and your function should return the index number 2.
-Example 2:
-
-Input: nums = [1,2,1,3,5,6,4]
-Output: 5
-Explanation: Your function can return either index number 1 where the peak element is 2, or index number 5 where the peak element is 6.
- 
-
-Constraints:
-
-1 <= nums.length <= 1000
--231 <= nums[i] <= 231 - 1
-nums[i] != nums[i + 1] for all valid i."""
-
-from typing import List
-
-class Solution:
-    def findPeakElement(self, nums: List[int]) -> int:
-        left, right = 0, len(nums) - 1
-
-        while left < right:
-            mid = (left + right) // 2
-            
-            if nums[mid] < nums[mid + 1]:
-                left = mid + 1
-            else:
-                right = mid
+class GraphPractice:
+    def __init__(self, nodes) -> None:
+        self.nodes = nodes
+        self.adj_matrix = [[0 for _ in range(nodes)] for _ in range(nodes)]
         
-        return left
+    def add_edge(self, node1, node2):
+        self.adj_matrix[node1][node2] = 1
+        self.adj_matrix[node2][node1] = 1
+        
+    def remove_edge(self, node1, node2):
+        self.adj_matrix[node1][node2] = 0
+        self.adj_matrix[node2][node1] = 0
+        
+    def display_graph(self):
+        for i in range(self.nodes):
+            for j in range(i+1, self.nodes):
+                if self.adj_matrix[i][j] == 1:
+                    print(f"connected edge between node {i} and {j}")
+    
+    def display_matrix(self):
+        for row in self.adj_matrix:
+            print(row)
+        print()
+    
+graph1 = GraphPractice(5)
+graph1.display_matrix()
+
+graph1.add_edge(2, 4)
+graph1.display_matrix()
+
+graph1.display_graph()
