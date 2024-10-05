4bfb1e9d2debd91302b4b20ac56f0ee2ee556134
Soroush Kami <soroushkami10@gmail.com>
2024-10-05 13:41:23 -0400

    Graph valid tree problem - brute force

--- Main.py
+++ Main.py
@@ -1,6 +1,9 @@
-"""Connected Components
-Problem Statement: Given an undirected graph, count the number of connected components.
+"""Graph Valid Tree
+Problem Summary
+A graph is a valid tree if:
 
+It is connected (there is a path between every pair of vertices).
+It contains no cycles.
 """
 
 from typing import List
@@ -9,11 +12,13 @@ class Graph:
     def __init__(self, nodes) -> None:
         self.nodes = nodes
         self.adj_matrix = [[0 for _ in range(nodes)] for _ in range(nodes)]
-    
+        
+        
     def add_edge(self, node1, node2):
         self.adj_matrix[node1][node2] = 1
         self.adj_matrix[node2][node1] = 1
         
+    
     def dfs_recursive(self, start, visited: List[int]):
         visited.append(start)
         
@@ -21,20 +26,49 @@ class Graph:
             if self.adj_matrix[start][i] == 1 and i not in visited:
                 self.dfs_recursive(i, visited)
                 
-    def count_connected_components(self):
-        visited = []
+    
+    def count_component(self):
         counter = 0
+        visited = []
         
         for i in range(self.nodes):
             if i not in visited:
-                self.dfs_recursive(i, visited)
                 counter += 1
-        
-        return print(counter)
+                self.dfs_recursive(i, visited)
+        return counter
     
-graph1 = Graph(6)
+    
+    def check_cycle(self, current, parent, visited: List[int]):
+        visited.append(current)
+        
+        for i in range(self.nodes):
+            if self.adj_matrix[current][i] == 1:
+                if i not in visited:
+                    if self.check_cycle(i, current, visited):
+                        return True
+                elif i != parent:
+                    return True
+        return False
+        
+                
+    def check_valid_graph(self):
+        visited = []
+
+        if self.count_component() == 1 and self.check_cycle(0, -1, visited) == 0 :
+            return True
+        else:
+            return False 
+        
+
+    
+
+graph1 = Graph(5)
 graph1.add_edge(2, 0)
 graph1.add_edge(2, 3)
 graph1.add_edge(4, 0)
-graph1.count_connected_components()
+graph1.add_edge(1, 2)
+graph1.add_edge(1, 3)
+
+print(graph1.check_valid_graph()) 
+    
     
