4f4b56290aff34fe8869bd6bd1e65e005d41fe25
Soroush Kami <soroushkami10@gmail.com>
2024-10-19 02:56:35 -0400

    Clean code - remove redundant else

--- Stack.py
+++ Stack.py
@@ -8,28 +8,29 @@ class Stack:
         if isinstance(value, list):
             for i in value:
                 self.push(i)
-        else:
-            self.stack.append(value)
-            if not self.min_stack or value <= self.min_stack[-1]:
-                self.min_stack.append(value)
-            if not self.max_stack or value >= self.max_stack[-1]:
-                self.max_stack.append(value)
+                
+        self.stack.append(value)
+        if not self.min_stack or value <= self.min_stack[-1]:
+            self.min_stack.append(value)
+        if not self.max_stack or value >= self.max_stack[-1]:
+            self.max_stack.append(value)
         
     def pop(self):
         if self.isEmpty():
             return None
-        else:
-            if self.peek() == self.min_stack[-1]:
-                self.min_stack.pop()
-            if self.peek() == self.max_stack[-1]:
-                self.max_stack.pop()
-            return self.stack.pop()
+        
+        value = self.peek()
+        if value == self.min_stack[-1]:
+            self.min_stack.pop()
+        if value == self.max_stack[-1]:
+            self.max_stack.pop()
+        return self.stack.pop()
     
     def peek(self):
         if self.isEmpty():
             return None
-        else:
-            return self.stack[-1]
+        
+        return self.stack[-1]
         
     def get_min(self):
         return self.min_stack[-1] if self.min_stack else None
