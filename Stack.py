c5c1d6c9d8b8d601a0dacb361ca0190aef59256f
Soroush Kami <soroushkami10@gmail.com>
2024-10-19 02:55:14 -0400

    Implement min/max stack

--- Stack.py
+++ Stack.py
@@ -1,17 +1,28 @@
 class Stack:
     def __init__(self) -> None:
         self.stack = []
+        self.min_stack = []
+        self.max_stack = []
 
     def push(self, value):
         if isinstance(value, list):
-            self.stack.extend(value)
+            for i in value:
+                self.push(i)
         else:
             self.stack.append(value)
+            if not self.min_stack or value <= self.min_stack[-1]:
+                self.min_stack.append(value)
+            if not self.max_stack or value >= self.max_stack[-1]:
+                self.max_stack.append(value)
         
     def pop(self):
         if self.isEmpty():
             return None
         else:
+            if self.peek() == self.min_stack[-1]:
+                self.min_stack.pop()
+            if self.peek() == self.max_stack[-1]:
+                self.max_stack.pop()
             return self.stack.pop()
     
     def peek(self):
@@ -19,6 +30,12 @@ class Stack:
             return None
         else:
             return self.stack[-1]
+        
+    def get_min(self):
+        return self.min_stack[-1] if self.min_stack else None
+    
+    def get_max(self):
+        return self.max_stack[-1] if self.max_stack else None
     
     def isEmpty(self):
         return len(self.stack) == 0
