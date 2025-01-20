677592e2d8f96c2607892cb0968bdd42e7c26202
Soroush Kami <soroushkami10@gmail.com>
2025-01-20 10:30:16 -0500

    nc - A&H: contain duplicates

--- Main.py
+++ Main.py
@@ -1,42 +1,12 @@
-"""Given an array of integers arr, return true if the number of occurrences of each value in the array is unique or false otherwise.
-
- 
-
-Example 1:
-
-Input: arr = [1,2,2,1,1,3]
-Output: true
-Explanation: The value 1 has 3 occurrences, 2 has 2 and 3 has 1. No two values have the same number of occurrences.
-Example 2:
-
-Input: arr = [1,2]
-Output: false
-Example 3:
-
-Input: arr = [-3,0,1,-3,1,1,1,-3,10,0]
-Output: true
- 
-
-Constraints:
-
-1 <= arr.length <= 1000
--1000 <= arr[i] <= 1000"""
-
-from typing import List
+"""Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct."""
 
 class Solution:
-    def uniqueOccurrences(self, arr: List[int]) -> bool:
-        seen = {}
+    def containsDuplicate(self, nums: List[int]) -> bool:
 
-        for i in range(len(arr)):
-            if arr[i] not in seen:
-                seen[arr[i]] = 0
-                continue
-            seen[arr[i]] += 1
-        
-        list = seen.values()
-        if len(list) == len(set(list)):
-            return True
-        else:
+        nums2 = set(nums)
+
+        if len(nums) == len(nums2):
             return False
-                
+        else:
+            return True
+        
