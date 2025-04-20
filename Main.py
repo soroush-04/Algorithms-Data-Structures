458d588a5bacc3273a00a9019160c3f0af0aa69d
Soroush Kami <soroushkami10@gmail.com>
2025-04-19 22:27:50 -0400

    Coin change DP

--- Main.py
+++ Main.py
@@ -1,24 +1,21 @@
 """
-climbing stairs
-
-You are climbing a staircase. It takes n steps to reach the top.
-Each time you can either climb 1 or 2 steps.
-how many distinct ways can you climb to the top?
+Coin Change
 """
+
+from typing import List
 
 
 class Solution:
-    def climb_stairs(self, n: int) -> int:
-        a, b = 1, 2
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [float("inf")] * (amount + 1)
+        dp[0] = 0
 
-        if n == 1:
-            return 1
+        for i in range(1, amount + 1):
+            for coin in coins:
+                if i - coin >= 0:
+                    dp[i] = min(dp[i], dp[i - coin] + 1)
 
-        for _ in range(2, n):
-            a, b = b, a + b
-
-        return b
-
-
-test = Solution()
-print(test.climb_stairs(5))
+        if dp[amount] == float("inf"):
+            return -1
+        else:
+            return dp[amount]
