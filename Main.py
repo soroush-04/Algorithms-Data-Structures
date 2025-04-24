0361a8cff598fcebdb19cc0ed36b779f16610513
Soroush Kami <soroushkami10@gmail.com>
2025-04-24 10:25:41 -0400

    Longest increasing subsequence - DP - Binary Search

--- Main.py
+++ Main.py
@@ -1,16 +1,41 @@
 """
-Reverse Bits
+Longest Increasing Subsequence
 """
 
 from typing import List
 
 
+# class Solution:
+#     def lengthOfLIS(self, nums: List[int]) -> int:
+#         n = len(nums)
+#         dp = [1] * n
+
+#         for i in range(n):
+#             for j in range(i):
+#                 if nums[i] > nums[j]:
+#                     dp[i] = max(dp[i], dp[j] + 1)
+
+#         return max(dp)
+
+
 class Solution:
-    def reverseBits(self, n: int) -> int:
-        ans = 0
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        sub = []
 
-        for _ in range(32):
-            ans = (ans << 1) | (n & 1)
-            n >>= 1
+        def binary_search_left(arr, target):
+            left = 0
+            right = len(arr)
+            while left < right:
+                mid = (left + right) // 2
+                if arr[mid] < target:
+                    left = mid + 1
+                else:
+                    right = mid
+            return left
 
-        return ans
+        for num in nums:
+            i = binary_search_left(sub, num)
+            if i == len(sub):
+                sub.append(num)
+            else:
+                sub[i] = num
