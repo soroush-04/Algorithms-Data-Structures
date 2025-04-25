e53826bacde329657a8297d5f640bf9944ca2b17
Soroush Kami <soroushkami10@gmail.com>
2025-04-24 23:20:40 -0400

    GCD of strings

--- Main.py
+++ Main.py
@@ -1,41 +1,20 @@
 """
-Longest Increasing Subsequence
+Greatest Common Divisor of Strings
 """
 
 from typing import List
 
 
-# class Solution:
-#     def lengthOfLIS(self, nums: List[int]) -> int:
-#         n = len(nums)
-#         dp = [1] * n
-
-#         for i in range(n):
-#             for j in range(i):
-#                 if nums[i] > nums[j]:
-#                     dp[i] = max(dp[i], dp[j] + 1)
-
-#         return max(dp)
-
-
 class Solution:
-    def lengthOfLIS(self, nums: List[int]) -> int:
-        sub = []
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        if str1 + str2 != str2 + str1:
+            return ""
 
-        def binary_search_left(arr, target):
-            left = 0
-            right = len(arr)
-            while left < right:
-                mid = (left + right) // 2
-                if arr[mid] < target:
-                    left = mid + 1
-                else:
-                    right = mid
-            return left
+        def gcd(a, b):
+            while b:
+                a, b = b, a % b
+            return a
 
-        for num in nums:
-            i = binary_search_left(sub, num)
-            if i == len(sub):
-                sub.append(num)
-            else:
-                sub[i] = num
+        gcd_len = gcd(len(str1), len(str2))
+
+        return str1[:gcd_len]
