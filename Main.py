5f84ba94b7e8e22736fdb7071ef508c01b43d68f
Soroush Kami <soroushkami10@gmail.com>
2025-04-25 17:54:22 -0400

    Maximum Depth of Binary Tree

--- Main.py
+++ Main.py
@@ -1,20 +1,19 @@
 """
-Greatest Common Divisor of Strings
+Maximum Depth of Binary Tree
 """
 
-from typing import List
+from typing import List, Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
 
 class Solution:
-    def gcdOfStrings(self, str1: str, str2: str) -> str:
-        if str1 + str2 != str2 + str1:
-            return ""
-
-        def gcd(a, b):
-            while b:
-                a, b = b, a % b
-            return a
-
-        gcd_len = gcd(len(str1), len(str2))
-
-        return str1[:gcd_len]
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
