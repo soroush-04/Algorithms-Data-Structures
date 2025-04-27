6b2b4f511f1f88b08cd531b2f88e2ed0ae2cdf70
Soroush Kami <soroushkami10@gmail.com>
2025-04-27 13:46:18 -0400

    Sqrt(x) with binary search

--- Main.py
+++ Main.py
@@ -1,34 +1,26 @@
 """
-Meeting Rooms
-Given an array of meeting time interval objects consisting of start and end times [[start_1,end_1],[start_2,end_2],...] (start_i < end_i), determine if a person could add all meetings to their schedule without any conflicts.
-
-Example 1:
-
-Input: intervals = [(0,30),(5,10),(15,20)]
-
-Output: false
+Sqrt(x)
 """
 
 from __future__ import annotations
 from typing import List, Optional
 
 
-class Interval:
-    def __init__(self, start, end):
-        self.start = start
-        self.end = end
+def sqrt(x):
+    l = 0
+    r = x
 
-    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
-        intervals.sort(key=lambda x: x.start)
+    while l <= r:
+        m = (l + r) // 2
 
-        for i in range(1, len(intervals)):
-            if intervals[i].start < intervals[i - 1].end:
-                return False
+        if m * m == x:
+            return m
+        elif m * m > x:
+            r = m - 1
+        else:
+            l = m + 1
 
-        return True
+    return r
 
 
-intervals = [Interval(0, 30), Interval(5, 10), Interval(15, 20)]
-
-solution = Interval(0, 0)
-print(solution.canAttendMeetings(intervals))
+print(sqrt(8))
