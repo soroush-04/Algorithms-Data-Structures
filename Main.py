47d52d9606c4062a05a5fdbf0cc352108c6ddd08
Soroush Kami <soroushkami10@gmail.com>
2025-05-17 22:31:16 -0400

    Combination Sum - backtrcack

--- Main.py
+++ Main.py
@@ -1,5 +1,31 @@
 """
-1985. Find the Kth Largest Integer in the Array
+39. Combination Sum
+
+Given an array of distinct integers candidates and a target integer target, return a list of all unique combinations of candidates where the chosen numbers sum to target. You may return the combinations in any order.
+
+The same number may be chosen from candidates an unlimited number of times. Two combinations are unique if the frequency of at least one of the chosen numbers is different.
+
+The test cases are generated such that the number of unique combinations that sum up to target is less than 150 combinations for the given input.
+
+
+
+Example 1:
+
+Input: candidates = [2,3,6,7], target = 7
+Output: [[2,2,3],[7]]
+Explanation:
+2 and 3 are candidates, and 2 + 2 + 3 = 7. Note that 2 can be used multiple times.
+7 is a candidate, and 7 = 7.
+These are the only two combinations.
+Example 2:
+
+Input: candidates = [2,3,5], target = 8
+Output: [[2,2,2,2],[2,3,3],[3,5]]
+Example 3:
+
+Input: candidates = [2], target = 1
+Output: []
+
 """
 
 from __future__ import annotations
@@ -7,11 +33,22 @@ from typing import List, Optional
 
 
 class Solution:
-    def reverse_words(self, s: str) -> str:
-        words = s.split()
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
 
-        return " ".join(words[::-1])
+        def backtrack(index, path, sum):
+            if sum == target:
+                return res.append(path[:])
+            if sum > target:
+                return
 
+            for i in range(index, len(candidates)):
+                sum += candidates[i]
+                path.append(candidates[i])
+                backtrack(i, path, sum)
+                path.pop()
+                sum -= candidates[i]
 
-test = Solution()
-print(test.reverse_words(" hello dear boss "))
+        backtrack(0, [], 0)
+
+        return res
