e4e7819ae1ece61a3bc0c1176b1c9e0a33d94de3
Soroush Kami <soroushkami10@gmail.com>
2025-05-20 23:55:30 -0400

    if two strings are close - no counter

--- Main.py
+++ Main.py
@@ -1,21 +1,39 @@
 """
-1004. Max Consecutive Ones III
-Given a binary array nums and an integer k, return the maximum number of consecutive 1's in the array if you can flip at most k 0's.
+1657. Determine if Two Strings Are Close
+
+Two strings are considered close if you can attain one from the other using the following operations:
+
+Operation 1: Swap any two existing characters.
+For example, abcde -> aecdb
+Operation 2: Transform every occurrence of one existing character into another existing character, and do the same with the other character.
+For example, aacabb -> bbcbaa (all a's turn into b's, and all b's turn into a's)
+You can use the operations on either string as many times as necessary.
+
+Given two strings, word1 and word2, return true if word1 and word2 are close, and false otherwise.
 
 
 
 Example 1:
 
-Input: nums = [1,1,1,0,0,0,1,1,1,1,0], k = 2
-Output: 6
-Explanation: [1,1,1,0,0,1,1,1,1,1,1]
-Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
+Input: word1 = "abc", word2 = "bca"
+Output: true
+Explanation: You can attain word2 from word1 in 2 operations.
+Apply Operation 1: "abc" -> "acb"
+Apply Operation 1: "acb" -> "bca"
 Example 2:
 
-Input: nums = [0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1], k = 3
-Output: 10
-Explanation: [0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1]
-Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
+Input: word1 = "a", word2 = "aa"
+Output: false
+Explanation: It is impossible to attain word2 from word1, or vice versa, in any number of operations.
+Example 3:
+
+Input: word1 = "cabbba", word2 = "abbccc"
+Output: true
+Explanation: You can attain word2 from word1 in 3 operations.
+Apply Operation 1: "cabbba" -> "caabbb"
+Apply Operation 2: "caabbb" -> "baaccc"
+Apply Operation 2: "baaccc" -> "abbccc"
+
 """
 
 from __future__ import annotations
@@ -23,18 +41,27 @@ from typing import List, Optional
 
 
 class Solution:
-    def longestOnes(self, nums: List[int], k: int) -> int:
-        left = 0
-        maxi = 0
-        zero_counter = 0
+    def closeStrings(self, word1: str, word2: str) -> bool:
+        if len(word1) != len(word2):
+            return False
 
-        for right in range(len(nums)):
-            if nums[right] == 0:
-                zero_counter += 1
+        if set(word1) != set(word2):
+            return False
 
-            while zero_counter > k:
-                if nums[right] == 0:
-                    zero_counter -= 1
-                left += 1
+        word1_freq = {}
 
-            maxi = max(maxi, right - left + 1)
+        for c in word1:
+            if c in word1_freq:
+                word1_freq[c] += 1
+            else:
+                word1_freq[c] = 1
+
+        word2_freq = {}
+
+        for c in word2:
+            if c in word2_freq:
+                word2_freq[c] += 1
+            else:
+                word2_freq[c] = 1
+
+        return sorted(word1_freq.values()) == sorted(word2_freq.values())
